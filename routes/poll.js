8bc12dc612221a2590d4c5b780bbdc3a26cbc493
qasimved <qasimved@gmail.com>
2020-08-10 02:13:42 +0500

    Resolved pusher beam

--- routes/poll.js
+++ routes/poll.js
@@ -17,30 +17,48 @@ router.get("/", (req, res) => {
   //   message: "hello world",
   // });
 
-  let beamsClient = new PushNotifications({
-    instanceId: "410ee95b-fffc-4c01-aaa5-d7760e0358cb",
-    secretKey:
-      "53BB2DA4F848423CE135F8F53BCEED7F379BE1F17D172CF17EB469EA2C4D4702",
+  // let beamsClient = new PushNotifications({
+  //   instanceId: "410ee95b-fffc-4c01-aaa5-d7760e0358cb",
+  //   secretKey:
+  //     "53BB2DA4F848423CE135F8F53BCEED7F379BE1F17D172CF17EB469EA2C4D4702",
+  // });
+
+  // beamsClient
+  //   .publishToInterests(["hello"], {
+  //     fcm: {
+  //       notification: {
+  //         title: "Hello",
+  //         body: "Hello, world!",
+  //       },
+  //       priority: "high",
+  //     },
+  //   })
+  //   .then((publishResponse) => {
+  //     console.log("Just published:", publishResponse.publishId);
+  //   })
+  //   .catch((error) => {
+  //     console.log("Error:", error);
+  //   });
+
+  // res.json({ msg: "Message sent to android" });
+
+  const beamsClient = new PushNotifications({
+    instanceId: '92aa13be-5600-45b4-9904-62fc7d5927f2',
+    secretKey: 'DB177A806BB72C73C9C2886A632C0ECFD29C91CB504EEE24FC3ABA955AC81FDF'
   });
-
-  beamsClient
-    .publishToInterests(["hello"], {
-      fcm: {
-        notification: {
-          title: "Hello",
-          body: "Hello, world!",
-        },
-        priority: "high",
-      },
-    })
-    .then((publishResponse) => {
-      console.log("Just published:", publishResponse.publishId);
-    })
-    .catch((error) => {
-      console.log("Error:", error);
-    });
-
-  res.json({ msg: "Message sent to android" });
+  
+  beamsClient.publishToInterests(['debug-hello'], {
+    fcm: {
+      notification: {
+        title: 'Hello',
+        body: 'Hello, world!'
+      }
+    }
+  }).then((publishResponse) => {
+    console.log('Just published:', publishResponse.publishId);
+  }).catch((error) => {
+    console.error('Error:', error);
+  });
 });
 
 router.get("/getdata", (req, res) => {
