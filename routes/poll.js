ee173a171d29f710335d44e56f447c8073fb5fd2
zeeshan mushtaq <zeeshanmushtaq76@gmail.com>
2020-12-06 18:01:44 +0500

    upload data api complete

--- routes/poll.js
+++ routes/poll.js
@@ -6,87 +6,62 @@ const PushNotifications = require("@pusher/push-notifications-server");
 const firebase = require("../config/firebase");
 const { check, validationResult } = require("express-validator");
 const { CLIENT_RENEG_LIMIT } = require("tls");
+const fs = require('fs');
+const path = require("path");
 
-// @route   PUT /api/posts/post-image/upload
+// @route   Post /api/poll/uploadimage/
 // @desc    Upload post image
 // @access  Private
-router.post("/uploadimage", async (req, res) => {
-  if (req.files === null) {
-    return res.status(400).json({ msg: "No file uploaded" });
-  }
-  const path = require("path");
-  const os = require("os");
-  const imageToBeUploaded = {};
+router.post("/uploaddata", async (req, res) => {
 
-  console.log("file came here =>");
-  // console.log(req.body.image.name);
-  console.log(req.body);
+  const imageName = "hiddenimage.png";
+  
+  console.log("file came here");
+  
+  const imageUrl = `https://firebasestorage.googleapis.com/v0/b/secroicy-b5ba8.appspot.com/o/${imageName}?alt=media`;
+  
+  const { email, longitude, latitude, time, encodedimage } = req.body;
+  
+   // Convert base64 to buffer
+    const buffer = Buffer.from(encodedimage, "base64");
 
-  const file = req.body.image;
-  const fileExtension = file.mimetype.split("/")[1];
-  const imageUrl = `https://firebasestorage.googleapis.com/v0/b/secroicy-27f10.appspot.com/o/${file.name}?alt=media`;
+    const imagePath = path.resolve(__dirname, imageName);
 
-  // const filepath = path.join(os.tmpdir(), file.name.toString());
-  // const filepath = path.join(
-  //   "C:\\Users\\qasim\\OneDrive\\Pictures\\",
-  //   file.name.toString()
-  // );
+    // Pipes an image with "hiddenImage.jpg" as the name. 
+     fs.writeFileSync(imagePath, buffer);
+    console.log(path.resolve(__dirname, imageName))
+    var storageref = firebase.storage().bucket();
+    
+    console.log("fsfile " + fs.__dirname);   
+    console.log(buffer);
 
-  var storageref = firebase.storage().bucket();
-  const metadata = {
-    contentType: file.mimetype,
-  };
   storageref
-    .upload(req.files.file)
-    .then((snapshot) => {})
-    .then(() => {
+    .upload(imagePath)
+    .then((snapshot) => { console.log("snapsot "); console.log(snapshot)})
+    .then(() => { 
       return firebase
         .firestore()
         .collection("mobilelocation")
-        .add({ imageUrl, createdAt: new Date().toISOString() });
+        .add({ 
+          email,
+          longitude,
+          latitude,
+          time,
+          imageUrl,
+          createdAt: new Date().toISOString()
+         });
     })
     .then(() => {
       return res.json({
-        message: `image uploaded successfully ${req.files.file.name}`,
+        message: `image uploaded successfully ${fs}`,
       });
     })
     .catch((err) => {
       console.error(err);
-      console.error(req.files.file);
-      console.error(req.files.file.name);
-      return res.status(500).json({ error: req.files.file });
+      return res.status(500).json({ error: req });
     });
 });
 
-// router.get("/:email", (req, res) => {
-
-//   const email = req.params.email;
-//   console.log(email, " email;;" )
-//   let beamsClient = new PushNotifications({
-//     instanceId: "410ee95b-fffc-4c01-aaa5-d7760e0358cb",
-//     secretKey:
-//       "53BB2DA4F848423CE135F8F53BCEED7F379BE1F17D172CF17EB469EA2C4D4702",
-//   });
-
-//   beamsClient
-//     .publishToInterests(["hello"], {
-//       fcm: {
-//         notification: {
-//           title: "Hello",
-//           body: "Hello, world!",
-//         },
-//         priority: "high",
-//       },
-//     })
-//     .then((publishResponse) => {
-//       console.log("Just published:", publishResponse.publishId);
-//     })
-//     .catch((error) => {
-//       console.log("Error:", error);
-//     });
-//   console.log("sent to android push notification (poll.file)");
-//   res.json({ msg: `Message sent to android ${email}` });
-// });
 
 router.get("/getdata", (req, res) => {
   pusher.trigger("my-channel", "my-event-1", "my-channel");
